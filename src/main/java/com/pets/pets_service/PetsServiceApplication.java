19c90702f479c74923d9f96598bdd7e867a09a17
Yasmine Masmoudi <87315006+yasminemasmoudi@users.noreply.github.com>
2023-02-03 10:46:42 +0100

    first

---
+++ src/main/java/com/pets/pets_service/PetsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pets.pets_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pets/pets_service/PetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pets.pets_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
