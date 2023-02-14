cd2065b017769a31bf96c22d9f5169c2fc6f07a6
Yasmine Masmoudi <87315006+yasminemasmoudi@users.noreply.github.com>
2023-02-14 22:14:31 +0100

    2

--- src/main/java/com/pets/pets_service/Repositories/ContractTypeRepo.java
+++ src/main/java/com/pets/pets_service/Repositories/ContractTypeRepo.java
@@ -1,10 +1,11 @@
 package com.pets.pets_service.Repositories;
-import com.pets.pets_service.Models.Species;
+
+import com.pets.pets_service.Models.ContractType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200/")
 @Repository
-public interface SpeciesRepo extends JpaRepository<Species,Integer> {
+public interface ContractTypeRepo extends JpaRepository<ContractType,Integer> {
 }
--- src/main/java/com/pets/pets_service/Repositories/PersonRepo.java
+++ src/main/java/com/pets/pets_service/Repositories/PersonRepo.java
@@ -3,7 +3,9 @@ package com.pets.pets_service.Repositories;
 import com.pets.pets_service.Models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 @Repository
 public interface PersonRepo extends JpaRepository<Person,Integer> {
 }
