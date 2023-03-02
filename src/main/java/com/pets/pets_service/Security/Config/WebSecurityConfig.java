b65dbdd9f3f0b8bf200453f74fe5410c9af6f047
AymenKammoun <aymen.kammoun04@gmail.com>
2023-03-02 17:04:36 +0100

    aaaa

--- src/main/java/com/pets/pets_service/Security/Config/WebSecurityConfig.java
+++ src/main/java/com/pets/pets_service/Security/Config/WebSecurityConfig.java
@@ -5,6 +5,11 @@ import com.pets.pets_service.Service.VetService;
 import com.pets.pets_service.Service.PPService;
 
 import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,6 +18,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @AllArgsConstructor
@@ -27,25 +38,66 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/v*/clients/**")
-                .hasRole("CLIENT")
-                .antMatchers("/api/v*/veterinaries/**")
-                .hasRole("VET")
-                .antMatchers("/api/v*/productProviders/**")
-                .hasRole("PRODUCT_PROVIDER")
                 .antMatchers("/api/v*/*registration/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin().and()
+                .formLogin()
+                    .defaultSuccessUrl("/home")
+                    .successHandler(loginSuccessHandler())
+                    .failureHandler(loginFailureHandler())
+                    .permitAll()
+                .and()
                 .logout()
-                .logoutUrl("/api/v*/logout")
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");;
+                    .logoutUrl("/api/v*/logout")
+                    .logoutSuccessUrl("http://localhost:4200/")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID");
+        ;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler loginSuccessHandler() {
+        return (request, response, authentication) -> {
+            // custom logic to modify the response
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"succsess\":true}");
+        };
+    }
+
+    @Bean
+    public AuthenticationFailureHandler loginFailureHandler() {
+        return (request, response, exception) -> {
+            // custom logic to modify the response
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/json");
+            response.getWriter().write("{\"succsess\":false}");
+        };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        // add a specific path to the CORS configuration
+        CorsConfiguration pathConfig = new CorsConfiguration();
+        pathConfig.addAllowedOrigin("http://localhost:4200");
+        // pathConfig.addAllowedMethod("GET");
+        // pathConfig.addAllowedHeader("Authorization");
+
+        return source;
     }
 
     @Override
@@ -53,21 +105,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(clientDaoAuthenticationProvider());
         auth.authenticationProvider(vetDaoAuthenticationProvider());
         auth.authenticationProvider(ppDaoAuthenticationProvider());
-}
+    }
 
     @Bean
-     public DaoAuthenticationProvider ppDaoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
+    public DaoAuthenticationProvider ppDaoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(ppService);
         return provider;
-}
+    }
 
     @Bean
     public DaoAuthenticationProvider clientDaoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(clientService);
         return provider;
@@ -75,8 +125,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public DaoAuthenticationProvider vetDaoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(vetService);
         return provider;
